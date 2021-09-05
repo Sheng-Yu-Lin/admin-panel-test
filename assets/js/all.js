4712da43277540de10df898f66daa1b58fceef54
Sheng-Yu-Lin <87552065+Sheng-Yu-Lin@users.noreply.github.com>
2021-09-05 16:58:06 +0800

    Add files via upload

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,41 +1,23 @@
-ClassicEditor
-.create( document.querySelector( '#editor' ), {					
-toolbar: {
-  items: [
-    'heading',
-    'bold',
-    'italic',
-    'bulletedList',
-    'numberedList',
-    'imageUpload',
-    'link',
-    'code'
-  ]
-},
-language: 'zh',
-image: {
-  toolbar: [
-    'imageTextAlternative',
-    'imageStyle:inline',
-    'imageStyle:block',
-    'imageStyle:side'
-  ]
-},
-table: {
-  contentToolbar: [
-    'tableColumn',
-    'tableRow',
-    'mergeTableCells'
-  ]
-},
-  licenseKey: '',
-} )
-.then( editor => {
+"use strict";
+
+ClassicEditor.create(document.querySelector('#editor'), {
+  toolbar: {
+    items: ['heading', 'bold', 'italic', 'bulletedList', 'numberedList', 'imageUpload', 'link', 'code']
+  },
+  language: 'zh',
+  image: {
+    toolbar: ['imageTextAlternative', 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side']
+  },
+  table: {
+    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+  },
+  licenseKey: ''
+}).then(function (editor) {
   window.editor = editor;
-} )
-.catch( error => {
-  console.error( 'Oops, something went wrong!' );
-  console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
-  console.warn( 'Build id: bvqgxne878wi-76t13wj44qu0' );
-  console.error( error );
-} );
+})["catch"](function (error) {
+  console.error('Oops, something went wrong!');
+  console.error('Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:');
+  console.warn('Build id: bvqgxne878wi-76t13wj44qu0');
+  console.error(error);
+});
+//# sourceMappingURL=all.js.map
