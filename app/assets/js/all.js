b960874bcc818fb8708fc5e63c738899a59e6444
unknown <User@G0767-X390-2.CPDC.COM.TW>
2021-09-05 15:14:52 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,41 @@
+ClassicEditor
+.create( document.querySelector( '#editor' ), {					
+toolbar: {
+  items: [
+    'heading',
+    'bold',
+    'italic',
+    'bulletedList',
+    'numberedList',
+    'imageUpload',
+    'link',
+    'code'
+  ]
+},
+language: 'zh',
+image: {
+  toolbar: [
+    'imageTextAlternative',
+    'imageStyle:inline',
+    'imageStyle:block',
+    'imageStyle:side'
+  ]
+},
+table: {
+  contentToolbar: [
+    'tableColumn',
+    'tableRow',
+    'mergeTableCells'
+  ]
+},
+  licenseKey: '',
+} )
+.then( editor => {
+  window.editor = editor;
+} )
+.catch( error => {
+  console.error( 'Oops, something went wrong!' );
+  console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
+  console.warn( 'Build id: bvqgxne878wi-76t13wj44qu0' );
+  console.error( error );
+} );
